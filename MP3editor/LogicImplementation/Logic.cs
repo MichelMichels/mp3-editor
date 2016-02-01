fe96758d5332b1641f6fe8695e15872f38ad7882
Michel Michels <michel.michels@student.odisee.be>
2016-02-02 00:06:47 +0100

    reads ID3v2.3 frames

--- MP3editor/LogicImplementation/Logic.cs
+++ MP3editor/LogicImplementation/Logic.cs
@@ -23,7 +23,7 @@ namespace LogicImplementation
         public Logic()
         {
             // Subscribe to DataHandler events
-            data.BytesLoaded += (int count) => Console.WriteLine($"{count} bytes loaded.");
+            //data.BytesLoaded += (int count) => Console.WriteLine($"{count} bytes loaded.");
         }
 
         // ---------
@@ -34,9 +34,12 @@ namespace LogicImplementation
             // Create MP3 vars
             var songWrapper = new MP3info();
             var songHeader = new ID3Header();
+            var songFrames = new List<ID3Frame>();
                                    
             // Load header in byte array, header = 10 bytes
             var bytes = data.LoadBytes(fileName, 10);
+
+            WriteBytes(bytes);
 
             /////////////////////////////////////////////
             //
@@ -49,13 +52,6 @@ namespace LogicImplementation
             //
             /////////////////////////////////////////////
             try {
-                /*
-                if (new char[(char)bytes[0], (char)bytes[1], (char)bytes[2]] != new char['I', 'D', '3'])
-                {
-                    throw (new ArgumentException("File not supported"));
-                }
-                */
-
                 // set version
                 songHeader.MajorVersion = bytes[3];
                 songHeader.RevisionNumber = bytes[4];
@@ -69,7 +65,7 @@ namespace LogicImplementation
                 songHeader.TagSize = CalculateID3TagSize(new byte[] { bytes[6], bytes[7], bytes[8], bytes[9] });
 
                 // DEBUG
-                Console.WriteLine($"[DEBUG] Logic.cs 48");
+                Console.WriteLine($"[DEBUG] Logic.cs 72");
                 Console.WriteLine($"Tagsize: {songHeader.TagSize}");
 
                 // Add header to MP3info var
@@ -77,6 +73,21 @@ namespace LogicImplementation
 
                 // request further bytes from data layer
                 bytes = data.LoadBytes(fileName, songHeader.TagSize + 10);
+
+                // Load all the frames
+                int offset = 10;
+                while( offset < bytes.Count)
+                {
+                    ID3Frame fr = GetID3Frame(bytes, offset);
+                    if (fr.Size == 0) break;
+
+                    songFrames.Add(fr);
+                    offset += fr.Size + 10;
+                }
+
+                PrintFrames(songFrames);
+
+                songWrapper.Frames = songFrames;
 
                 // return object
                 return songWrapper;
@@ -116,6 +127,32 @@ namespace LogicImplementation
 
                 counter++;
                 
+            }
+        }
+
+        private ID3Frame GetID3Frame(List<byte> bytes, int start)
+        {
+            ID3Frame frame = new ID3Frame();
+
+            char[] c = { (char)bytes[start], (char)bytes[start + 1], (char)bytes[start + 2], (char)bytes[start + 3] };
+            frame.ID = new string(c);
+
+            // Calculate frame size
+            frame.Size = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                frame.Size += bytes[start + 4 + i] << 8 * (3 - i);
+            }
+
+            return frame;
+        }
+
+        public void PrintFrames(List<ID3Frame> frames)
+        {
+            foreach(var frame in frames )
+            {
+                Console.WriteLine($"{frame.ID}");
             }
         }
     }
